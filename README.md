0ad0b85e043fca8fa639af8f648c90dc41d4ccba
marcolivier rodrigue <marcolivier.rodrigue@gmail.com>
2022-01-16 18:31:15 -0500

    can determine weither or not a number is prime

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MillerRabinTestBiggestLiars
+This project was inspired by this video: https://www.youtube.com/watch?v=_MscGSN5J6o&t=33s&ab_channel=Numberphile
+Where Matt Parker asked to find the numbers that lie the most often.
+
+Special thanks to: https://fr.wikipedia.org/wiki/Test_de_primalit%C3%A9_de_Miller-Rabin
--- main.cpp
+++ main.cpp
@@ -1,6 +1,125 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <omp.h>
+#include <vector>
+
+char* dataPath = "../../data/";
+const char* isPrimeFile = "isPrime.txt";
+const char* liarCounter = "liars.txt";
+
+bool isDevider(int b, int c)
+{
+    return b % c == 0;
+}
+
+bool isOdd(int c)
+{
+    return !isDevider(c, 2);
+}
+
+//  Assumes that c is always even
+int getHighestPowerOf2(int c) 
+{
+    return (c & (~(c - 1)));
+}
+
+//  Assumes that c is always a power of 2
+int getExponentOf2(int c)
+{
+    return log(c) / log(2);
+}
+
+/*
+*   We want to prepare our number n for court:  n = 2^m * d + 1
+*   which is the equivalent to:                 (n - 1) / 2^m = d
+*
+*   Note: d will always be  odd
+*/
+void PrepareN(const int n, int& d, int& m)
+{
+    int c = getHighestPowerOf2(n - 1);
+    d = (n - 1) / c;
+    m = getExponentOf2(c);
+}
+
+//  Used to evaluate if a suspect is really prime or not
+std::vector<int> getStarWitnesses(int n)
+{
+    if (n < 1373653)
+    {
+        return { 2, 3 };
+    }
+    else if(n < 9080191) 
+    {
+        return { 31, 73 };
+    }
+    else if (n < 25326001)
+    {
+        return { 2, 3, 5 };
+    }
+    else 
+    {
+        return { 2, 13, 23, 1662803 };
+    }
+}
+
+
+bool TestWithness_MillerRabin(int n, int a)
+{
+    int d, m;
+    PrepareN(n, d, m);
+    int x = pow(a, d);
+    if (x == 1 || x == n - 1)
+    {
+        return true;
+    }
+    for(int i = 0; i < m-1; i++)
+    {
+        x = (int)pow(x, 2) % n;
+        if(x = n-1)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool isPrime(int n) 
+{
+    std::vector<int> starWithness = getStarWitnesses(n);
+    for (auto it = starWithness.begin(); it != starWithness.end(); it++) 
+    {
+        if (!TestWithness_MillerRabin(n, *it))
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 
 int main () 
 {
+    /*
+    std::ofstream myfile;
+    myfile.open(strcat(dataPath, "isPrime.txt"));
+    myfile << "2:1\n3:1";
+    myfile.close();
+
+    int initNumb = 5;
+    */
+    std::cout << isPrime(10) << std::endl;
+    /*
+        #pragma omp parallel for
+        for (int i = initNumb; i < std::numeric_limits<int>::max(); i += 2)
+        {
+            std::vector<int> starWithness = getStarWitnesses(i);
+        }
+    */
+
+
+    
     return 0;
 }
